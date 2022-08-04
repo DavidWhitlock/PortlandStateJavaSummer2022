e96b8fbcdf339ec4c6ca26ab01753d5c8bfaf7f8
David Whitlock <david.m.whitlock@gmail.com>
2022-08-03 18:31:25 -0700

    Print the sum when the equals button is clicked.

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/CalculatorActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/CalculatorActivity.java
@@ -3,6 +3,9 @@ package edu.pdx.cs410j.whitlock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+    }
+
+    public void computeSum(View view) {
+        EditText leftOperand = findViewById(R.id.leftOperand);
+        EditText rightOperand = findViewById(R.id.rightOperand);
+        EditText sum = findViewById(R.id.sum);
+
+        int left = Integer.parseInt(leftOperand.getText().toString());
+        int right = Integer.parseInt(rightOperand.getText().toString());
+
+        sum.setText(String.valueOf(left + right));
     }
 }

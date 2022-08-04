7265680dc6438555428b316cb805c6e6cac601d3
David Whitlock <david.m.whitlock@gmail.com>
2022-08-03 18:59:22 -0700

    Display a list of integers in the MainActivity.

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/MainActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.sums);
+
+        ArrayAdapter<Integer> sums = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        sums.addAll(1, 2, 3, 4, 5);
+        listView.setAdapter(sums);
     }
 
     public void launchCalculator(View view) {
